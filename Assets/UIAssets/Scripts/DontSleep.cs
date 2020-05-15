74893f736b1a8e7ca6cf2d6f2cd58c74329793f1
callFEELD <callfeeld@gmail.com>
2020-05-15 10:08:13 +0200

    Disabled smartphone screen sleep & updated Overlay for HD devices

---
+++ Assets/UIAssets/Scripts/DontSleep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontSleep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
