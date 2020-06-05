e2e82dbd9cdfa263b48e9e232ded3302a2dccafb
callFEELD <callfeeld@gmail.com>
2020-06-05 17:03:17 +0200

    removed assets

--- Assets/UIAssets/Scripts/MenuScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
-
-public class MenuScript : MonoBehaviour { 
-    public void ChangeScene(string sceneName) {
-        SceneManager.LoadScene(sceneName); 
-    } 
-}
